ca0db553210e95bd3853d5fd91bbedbaa691922f
sreejith1996 <pisharodysreejith96@gmail.com>
2021-03-09 21:35:07 -0500

    changed IP to 0.0.0.0

--- main.py
+++ main.py
@@ -22,4 +22,4 @@ def make_dwarf_name():
 #   return "Pinging model application"
 
 if __name__ == '__main__':
-    app.run(debug=True, host='127.0.0.1', port=8080)
+    app.run(debug=True, host='0.0.0.0', port=9696)
